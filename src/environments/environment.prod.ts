edaa58a35e9da8dc012f30063e587a4169d8a61b
stekosto <stekosto2@gmail.com>
2019-01-17 18:55:11 -0600

    correct enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDwgekiI_Rc39ZgnW2vXBX6-eNC5U9ijP8',
+    authDomain: 'job-board-c106a.firebaseapp.com',
+    databaseURL: 'https://job-board-c106a.firebaseio.com',
+    projectId: 'job-board-c106a',
+    storageBucket: 'job-board-c106a.appspot.com',
+    messagingSenderId: '417296488174'
+  }
 };
